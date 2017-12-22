c2baae5ee033657272fcfea863d27f1704576e4c
sachiyuki <sachiyuki@outlook.jp>
2017-12-22 21:51:07 +0900

    12/22

---
+++ buy/workspace/ecsite2/src/com/internousdev/ecsite/dao/AdminItemDAO.java
@@ -0,0 +1,12 @@
+package com.internousdev.ecsite.dao;
+
+import com.internousdev.ecsite.dto.AdminItemDTO;
+
+public class AdminItemDAO {
+
+	public AdminItemDTO getAdminItemInfo() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+}
---
+++ buy/workspace/ecsite2/src/com/internousdev/ecsite/dto/AdminItemDTO.java
@@ -0,0 +1,26 @@
+package com.internousdev.ecsite.dto;
+
+public class AdminItemDTO {
+
+	public AdminItemDTO getAdminItemInfo() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public Object getId() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public Object getItemName() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+	public Object getItemPrice() {
+		// TODO 自動生成されたメソッド・スタブ
+		return null;
+	}
+
+
+}
